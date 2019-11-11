63cf169fdb6b6786f11cd530fd450178fafd5294
maryam <it.lancer999@gmail.com>
2019-11-11 15:30:18 -0500

    added preloader fadeOut function

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
+
+
 jQuery(document).ready(function(){
+
+   jQuery(window).on("load", function(){
+    jQuery("#preloader").fadeOut("slow");
+  });
+
 	jQuery('.skillbar').each(function(){
 		jQuery(this).find('.skillbar-bar').animate({
 			width:jQuery(this).attr('data-percent')
 		},6000);
 	});
+
+ 
+
+ 
 });
